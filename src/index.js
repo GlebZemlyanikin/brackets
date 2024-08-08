dc569f301013a6030504436740ce42994540597a
Gleban152 <zemlyanikin.g.96@mail.ru>
2024-08-08 18:49:21 +0300

    Выполнение задания

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	const stack = [];
+	const bracketsMap = new Map(bracketsConfig);
+
+	for (let char of str) {
+		if (bracketsMap.has(char) && bracketsMap.get(char) === char) {
+			if (stack.length > 0 && stack[stack.length - 1] === char) {
+				stack.pop();
+			} else {
+				stack.push(char);
+			}
+		} else if (bracketsMap.has(char)) {
+			stack.push(char);
+		} else {
+			if (stack.length === 0 || bracketsMap.get(stack.pop()) !== char) {
+				return false;
+			}
+		}
+	}
+
+	return stack.length === 0;
+};
